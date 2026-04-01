5f5289b9218fc072d2d60e647e61eac7a784c7a9
Sevinc <sevincquliyeva000@gmail.com>
2026-04-01 16:25:50 +0400

    lab 2 done

--- demo/src/main/java/az/lab3/demo/repository/FlightBookingRepo.java
+++ demo/src/main/java/az/lab3/demo/repository/FlightBookingRepo.java
@@ -1,9 +1,12 @@
 package az.lab3.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import az.lab3.demo.entity.FlightBooking;
 
+
+@Repository
 public interface FlightBookingRepo extends JpaRepository<FlightBooking, Integer>{
 
 }
--- demo/src/main/java/az/lab3/demo/repository/FlightRepo.java
+++ demo/src/main/java/az/lab3/demo/repository/FlightRepo.java
@@ -1,9 +1,11 @@
 package az.lab3.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import az.lab3.demo.entity.Flight;
 
+@Repository
 public interface FlightRepo extends JpaRepository<Flight, Integer>{
 
 }
